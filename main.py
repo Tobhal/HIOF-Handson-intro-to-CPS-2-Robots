e7bc54f2904caa04515fac608e2b82ff9bc7e5e8
Tobias Hallingstad <tobias@hallingstad.me>
2022-11-28 12:34:36 +0100

    Trying to implement new move object feature

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from __future__ import annotations
+from typing import Callable, Optional
 
 import operator
 import time
-
-from typing import Callable, Optional
 
 from util import Vec2, Vec3, Pose, Object, RobotPickUp, Status
 from robot import Robot
 from Gripper import *
 from camera import Camera
 from conveyor import Conveyor
+from stack import Stack
 from threading import Thread
 
+# TODO: The robot is missing the correct block with 90 deg, like what happened with cam2 last time
 camera1 = Camera('10.1.1.8', Vec2(240, -170), Vec2(1.08, 1.08), Vec2(-1, -1),
                  (Vec2(14, 191), Vec2(450, 325)), 175,
                  [Object.CUBE, Object.CYLINDER])
@@ -34,7 +35,7 @@ cylinder = {  # TODO: Change to correct measurements
     'width': Vec3(0.06, 0.06, 0.07)
 }
 
-rob_1_cords = {
+rob1_cords = {
     'conveyor': Pose(0.015, 0.401, -0.022, rx=2.2, ry=-2.2),
     'idlePose': Pose(0.25, -0.12, 0.09),
     'object': {
@@ -45,7 +46,7 @@ rob_1_cords = {
     Object.CYLINDER: cylinder
 }
 
-rob_2_cords = {
+rob2_cords = {
     'conveyor': Pose(0.05, 0.399, -0.02, rx=2.2, ry=2.2),
     'idlePose': Pose(-0.25, -0.12, 0.09),
     'object': {
@@ -61,16 +62,27 @@ end_program = False
 rob1: Optional[Robot] = None
 rob2: Optional[Robot] = None
 
+rob1_place_stack = Stack(rob1_cords['object']['place'], Vec2(-1.0, 0.0), 2, Object.CUBE)
+rob1_conveyor_stack = Stack(rob1_cords['conveyor'])
+rob2_place_stack = Stack(rob2_cords['object']['place'], Vec2(-1.0, 0.0), 2, Object.CYLINDER)
+
 # noinspection PyBroadException
 try:
-    rob1 = Robot('10.1.1.6', 'rob1', rob_1_cords, use_rt=True)
-    rob2 = Robot('10.1.1.5', 'rob2', rob_2_cords, use_rt=True)
+    rob1 = Robot('10.1.1.6', 'rob1', Object.CUBE, rob1_cords, use_rt=True)
+    rob2 = Robot('10.1.1.5', 'rob2', Object.CYLINDER, rob2_cords, use_rt=True)
     Conveyor.robot = rob2
     Conveyor.lock = rob2.lock
 except:
     print('Error occurred with initializing robots. Exiting')
+    if rob1:
+        rob1.close()
+
+    if rob2:
+        rob2.close()
+
     end_program = True
     exit()
+    # os.execv(sys.argv[0], sys.argv)
 
 object_Pick_Up = RobotPickUp.NONE
 """Robot to pick up from conveyor"""
@@ -86,6 +98,24 @@ def termination_condition():
     return end_program or counter < 2
 
 
+# Pre robot move functions
+def pre_run1():
+    global rob1, rob2, camera1, camera2, object_move, object_Pick_Up
+    rob1_cubes, rob1_cylinders = camera1.get_shapes()
+
+    rob1_num_cubes, rob1_num_cylinders = len(rob1_cubes), len(rob1_cylinders)
+
+    rob2_cubes, rob2_cylinders = camera2.get_shapes()
+    rob2_num_cubes, rob2_num_cylinders = len(rob2_cubes), len(rob2_cylinders)
+
+    # Determen witch robot should move its object first
+    if rob1_num_cylinders > rob2_num_cubes:
+        object_move = RobotPickUp.R2
+    else:
+        object_move = RobotPickUp.R1
+
+
+# Robot move functions
 def move(rob: Robot, camera: Camera):
     """
     Moves the robot between two points
@@ -93,12 +123,12 @@ def move(rob: Robot, camera: Camera):
     print(f'{rob.name}: move to idle pos = {rob.cords["idlePose"]}')
     rob.move(rob.cords['idlePose'])
 
-    rob.move_object(rob.cords['object']['get'], rob.cords['conveyor'])
+    rob.object_move(rob.cords['object']['get'], rob.cords['conveyor'])
 
     print(f'{rob.name}: move to idle pos = {rob.cords["idlePose"]}')
     rob.move(rob.cords['idlePose'])
 
-    rob.move_object(rob.cords['conveyor'], rob.cords['object']['get'])
+    rob.object_move(rob.cords['conveyor'], rob.cords['object']['get'])
 
     rob.move(rob.cords['idlePose'])
 
@@ -145,7 +175,6 @@ def move3(rob: Robot, camera: Camera):
     while termination_condition():
         if object_Pick_Up.value == rob.name:
             rob.move_object_from_conveyor()
-            rob.cords['object']['place'].y += cube['size'].y + 0.01
 
             object_Pick_Up = RobotPickUp.NONE
             rob.move(rob.cords['idlePose'])
@@ -158,7 +187,7 @@ def move3(rob: Robot, camera: Camera):
             object_move = RobotPickUp.NONE
             rob.move(rob.cords['idlePose'])
 
-            rob.move_object(rob.cords['object']['get'], rob.cords['object']['place'], stop_at_idle=False)
+            rob.object_move(rob.cords['object']['get'], rob.cords['object']['place'], stop_at_idle=False)
             rob.cords['object']['place'].y += cube['size'].y + 0.01
             rob.cords['object']['get'].y -= cube['size'].y
 
@@ -168,11 +197,54 @@ def move3(rob: Robot, camera: Camera):
             print(f'Object Move = {object_move}')
             object_move = RobotPickUp.flip(RobotPickUp(rob.name))
 
-            rob.move_object(rob.cords['object']['get'], rob.cords['object']['place'], stop_at_idle=False)
+            rob.object_move(rob.cords['object']['get'], rob.cords['object']['place'], stop_at_idle=False)
             rob.cords['object']['place'].y += cube['size'].y + 0.01
             rob.cords['object']['get'].y -= cube['size'].y
 
             rob.move(rob.cords['idlePose'])
+
+
+def move4(rob: Robot, camera: Camera):
+    """
+    Move objects based on their position form the camera.
+    """
+    global object_Pick_Up, object_move, rob1, rob2
+
+    rob.move(rob.cords['idlePose'])
+    rob.send_program(rq_open())
+
+    # Wait for both robots to reach idle state before beginning
+    while rob1.status == Status.MOVING and rob2.status == Status.MOVING:
+        pass
+
+    while termination_condition():
+        objects = {
+            Object.CUBE: camera.get_cubes(),
+            Object.CYLINDER: camera.get_cylinders()
+        }
+
+        if object_Pick_Up.value == rob.name:
+            rob.move_object_from_conveyor(rob.object_store)
+
+            object_Pick_Up = RobotPickUp.NONE
+            rob.move(rob.cords['idlePose'])
+
+        if object_move.value == rob.name and len(objects[rob.object_move]) > 0:
+            rob.move_object_to_conveyor(objects[rob.object_move][0].to_vec3(), rob.object_move)
+
+            object_move = RobotPickUp.NONE
+            rob.move(rob.cords['idlePose'])
+
+        if object_move != rob.name and object_move != RobotPickUp.NONE and Conveyor.status == Status.READY and \
+                len(objects[rob.object_store]) > 0:
+            rob.move_object(objects[rob.object_store][0].to_vec3(), rob.cords['object']['place'])
+            rob.cords['object']['place'].y += rob.cords[rob.object_store]['size'].y + 0.01
+
+            rob.move(rob.cords['idlePose'])
+
+
+def move_simple(rob: Robot, camera: Camera):
+    pass
 
 
 def test_move(rob: Robot, camera: Camera):
@@ -279,8 +351,12 @@ def conveyor_move():
 # Other functions
 def main(move_func: Callable[[Robot, Camera], None],
          robots: list[tuple[Robot, Camera]],
-         conveyor_func: Callable = None,
+         conveyor_func: Callable[[], None] = None,
+         pre_run: Callable[[], None] = None,
          ):
+    if pre_run:
+        pre_run()
+
     threads: list[Thread] = list()
 
     for rob in robots:
@@ -296,23 +372,11 @@ def main(move_func: Callable[[Robot, Camera], None],
         thread.join()
 
 
-def test_main():
-    rob1_thread = Thread(target=test_move, args=(rob2, camera2,))
-
-    rob1_thread.start()
-
-    rob1_thread.join()
-
-
 if __name__ == '__main__':
     print('Program start')
 
     try:
-        # main(move)
-        # main(move2, conveyor_move)
-        # main(move3, conveyor_move)
-        # test_main()
-        main(test_move, [(rob1, camera1), (rob2, camera2)])
+        main(move4, [(rob1, camera1), (rob2, camera2)], conveyor_move, pre_run1)
     except KeyboardInterrupt:
         Conveyor.stop()
 
--- test.py
+++ test.py
@@ -2,27 +2,6 @@ import urllib.request
 import numpy as np
 import cv2
 
-def bg_remove(myimage):
-    # BG Remover 3
-    myimage_hsv = cv2.cvtColor(myimage, cv2.COLOR_BGR2HSV)
-
-    # Take S and remove any value that is less than half
-    s = myimage_hsv[:, :, 1]
-    s = np.where(s < 127, 0, 1)  # Any value below 127 will be excluded
-
-    # We increase the brightness of the image and then mod by 255
-    v = (myimage_hsv[:, :, 2] + 127) % 255
-    v = np.where(v > 100, 1, 0)  # Any value above 127 will be part of our mask
-
-    # Combine our two masks based on S and V into a single "Foreground"
-    foreground = np.where(s + v > 0, 1, 0).astype(np.uint8)  # Casting back into 8bit integer
-
-    background = np.where(foreground == 0, 255, 0).astype(np.uint8)  # Invert foreground to get background in uint8
-    background = cv2.cvtColor(background, cv2.COLOR_GRAY2BGR)  # Convert background back into BGR space
-    foreground = cv2.bitwise_and(myimage, myimage, mask=foreground)  # Apply our foreground map to original image
-    finalimage = background + foreground  # Combine foreground and background
-
-    return finalimage
 
 req = urllib.request.urlopen(f'http://10.1.1.8/LiveImage.jpg')
 # noinspection DuplicatedCode
@@ -31,12 +10,14 @@ img = cv2.imdecode(arr, -1)
 
 # img = cv2.imread('response camera 7 2.jpeg')
 
-""" camera 10.1.1.8
+ # camera 10.1.1.8
 x, y = 14, 191
 h, w = 450, 435
-"""
+""" # camera 10.1.1.7
 x, y = 0, 0
 h, w = 450, 477
+"""
+
 img = img[x:w, y:h]
 
 # img = bg_remove(img)
@@ -66,13 +47,15 @@ for cnt in contours:
         if 0.9 <= ratio <= 1.1:
             # img = cv2.drawContours(img, [cnt], -1, (0, 255, 255), 3)
             img = cv2.circle(img, (int(x + (w / 2)), int(y + (h / 2))), radius=2, color=(0, 0, 255), thickness=-1)
-            threshold = cv2.circle(threshold, (int(x + (w/2)), int(y + (h/2))), radius=2, color=(0, 0, 255), thickness=-1)
+            threshold = cv2.circle(threshold, (int(x + (w / 2)), int(y + (h / 2))), radius=2, color=(0, 0, 255),
+                                   thickness=-1)
             # cv2.putText(img, 'Square', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
         else:
             # cv2.putText(img, 'Rectangle', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
             img = cv2.circle(img, (int(x + (w / 2)), int(y + (h / 2))), radius=2, color=(0, 0, 255), thickness=-1)
             # img = cv2.drawContours(img, [cnt], -1, (0, 255, 0), 3)
-            threshold = cv2.circle(threshold, (int(x + (w/2)), int(y + (h/2))), radius=2, color=(0, 255, 0), thickness=-1)
+            threshold = cv2.circle(threshold, (int(x + (w / 2)), int(y + (h / 2))), radius=2, color=(0, 255, 0),
+                                   thickness=-1)
 
 # Detect cylinders
 blur = cv2.blur(gray, (3, 3))
@@ -183,7 +166,7 @@ for contour in contours:
 
 # displaying the image after drawing contours
 cv2.imshow('image', img)
-# cv2.imshow('threshold', threshold)
+cv2.imshow('threshold', threshold)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
