1e989f61805bc53063691e6c50089da9bff6ed7e
Tobias Hallingstad <tobias@hallingstad.me>
2022-11-28 13:56:48 +0100

    Added list to store prev positions

--- stack.py
+++ stack.py
@@ -7,10 +7,11 @@ from util import Vec2, Vec3, Object
 class Stack:
     current_height = 0
     padding = Vec2(0.1, 0.1)
+    prev_positions = []
 
-    def __init__(self, coords: Vec2 | Vec3, direction: Vec2 | Vec3, height: int, obj: Object):
+    def __init__(self, coords: Vec2 | Vec3, direction: Vec2, height: int, obj: Object):
         self.coords = coords if type(coords) is Vec3 else coords.to_vec3()
-        self.direction = direction if type(direction) is Vec3 else direction.to_vec3()
+        self.direction = direction
         self.height = height
         self.object = obj
 
@@ -25,22 +26,30 @@ class Stack:
 
         self.current_height = (self.current_height + 1) % self.height
 
-        self.coords.z = (self.object['size'].z * self.current_height)
+        self.coords.z = self.object['size'].z * self.current_height
 
         if self.current_height == 0:
-            self.coords = self.coords + self.padding + self.object['size']
+            self.coords = self.coords + self.padding + self.object['size'].to_vec2()
+
+        self.prev_positions.append(return_value * self.direction)
 
         return return_value * self.direction
+
+    def prev(self) -> Vec2:
+        return self.prev_positions.pop()
 
     def reset(self):
         self.coords = deepcopy(self.original_coords)
 
 
 if __name__ == '__main__':
-    stack = Stack(Vec2(0.0, 0.0), Vec2(-1.0, 1.0), 2, Object.CUBE)
+    stack = Stack(Vec2(0.0, 0.0), Vec2(1.0, 0.0), 2, Object.CUBE)
 
     print(stack.next())
     print(stack.next())
     print(stack.next())
-    stack.reset()
-    print(stack.next())
+
+    print(stack.prev())
+    print(stack.prev())
+    print(stack.prev())
+
